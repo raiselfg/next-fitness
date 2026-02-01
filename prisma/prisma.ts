e1de06948d30f574297df6d3972b8784452ffc25
raiselfg <raiselfg@mail.ru>
2026-02-01 03:36:51 +0300

    small prisma fix

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,6 +1,7 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client';
 import { Pool } from 'pg';
+
+import { PrismaClient } from '@/app/generated/prisma/client';
 
 const connectionString = `${process.env.POSTGRES_PRISMA_URL}`;
 
